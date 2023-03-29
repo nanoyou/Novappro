0056943df7427c2ba7349934e518fc19d6d27497
AkagawaTsurunaki <965145619@qq.com>
2023-03-29 17:10:46 +0800

    feat: 实现通过连接MySql数据库进行登录功能

--- src/main/java/com/github/akagawatsurunaki/novappro/service/LoginService.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/service/LoginService.java
@@ -1,10 +1,12 @@
 package com.github.akagawatsurunaki.novappro.service;
 
-import com.github.akagawatsurunaki.novappro.enumeration.VerifyCode;
+import com.github.akagawatsurunaki.novappro.mapper.impl.UserMapperImpl;
 import com.github.akagawatsurunaki.novappro.model.User;
 import com.github.akagawatsurunaki.novappro.test.UserTable;
 import lombok.Getter;
 import lombok.NonNull;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,12 +16,17 @@ public class LoginService {
     @Getter
     private static final LoginService instance = new LoginService();
 
-    /**
-     *
-     * @param username
-     * @param rawPassword
-     * @return
-     */
+    private static final UserMapperImpl USER_MAPPER = UserMapperImpl.getInstance();
+
+    public enum VerifyCode {
+        OK,
+        NO_SUCH_USER,
+        PASSWORD_ERROR,
+        FATAL_ERROR
+    }
+
+
+    @Deprecated
     public Map<String, Object> tryLoginWithUserName(@NonNull String username, @NonNull String rawPassword) {
         final String VERIFY_CODE = VerifyCode.class.getName();
         final String USER = User.class.getName();
@@ -28,7 +35,7 @@ public class LoginService {
 
         for (User user : UserTable.getUsers()) {
             if (user.getUsername().equals(username)) {
-                if(user.getRawPassword().equals(rawPassword)){
+                if (user.getRawPassword().equals(rawPassword)) {
                     result.put(VERIFY_CODE, VerifyCode.OK);
                     result.put(USER, user);
                 } else {
@@ -44,28 +51,43 @@ public class LoginService {
         return result;
     }
 
-    public Map<String, Object> tryLoginWithUserId(@NonNull Integer id, @NonNull String rawPassword){
-        final String VERIFY_CODE = VerifyCode.class.getName();
-        final String USER = User.class.getName();
+    public Pair<VerifyCode, User> tryLoginWithUserId(@NonNull Integer id, @NonNull String rawPassword) {
 
-        Map<String, Object> result = new HashMap<>();
-
-        for (User user : UserTable.getUsers()) {
-            if (user.getId().equals(id)) {
-                if(user.getRawPassword().equals(rawPassword)){
-                    result.put(VERIFY_CODE, VerifyCode.OK);
-                    result.put(USER, user);
-                } else {
-                    result.put(VERIFY_CODE, VerifyCode.PASSWORD_ERROR);
-                    result.put(USER, null);
-                }
-                return result;
-            }
+        User user = USER_MAPPER.getUserById(id);
+        // 判断用户是否存在
+        if (user == null) {
+            return new ImmutablePair<>(VerifyCode.NO_SUCH_USER, null);
         }
+        // 校验密码
+        if (user.getRawPassword().equals(rawPassword)) {
+            return new ImmutablePair<>(VerifyCode.OK, user);
+        }
+        return new ImmutablePair<>(VerifyCode.PASSWORD_ERROR, user);
 
-        result.put(VERIFY_CODE, VerifyCode.NO_SUCH_USER);
-        result.put(USER, null);
-        return result;
     }
+
+//    public Map<String, Object> tryLoginWithUserId(@NonNull Integer id, @NonNull String rawPassword){
+//        final String VERIFY_CODE = VerifyCode.class.getName();
+//        final String USER = User.class.getName();
+//
+//        Map<String, Object> result = new HashMap<>();
+//
+//        for (User user : UserTable.getUsers()) {
+//            if (user.getId().equals(id)) {
+//                if(user.getRawPassword().equals(rawPassword)){
+//                    result.put(VERIFY_CODE, VerifyCode.OK);
+//                    result.put(USER, user);
+//                } else {
+//                    result.put(VERIFY_CODE, VerifyCode.PASSWORD_ERROR);
+//                    result.put(USER, null);
+//                }
+//                return result;
+//            }
+//        }
+//
+//        result.put(VERIFY_CODE, VerifyCode.NO_SUCH_USER);
+//        result.put(USER, null);
+//        return result;
+//    }
 
 }
--- src/main/java/com/github/akagawatsurunaki/novappro/servlet/LoginServlet.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/LoginServlet.java
@@ -1,9 +1,8 @@
 package com.github.akagawatsurunaki.novappro.servlet;
 
-import com.github.akagawatsurunaki.novappro.enumeration.VerifyCode;
 import com.github.akagawatsurunaki.novappro.model.User;
 import com.github.akagawatsurunaki.novappro.service.LoginService;
-import com.github.akagawatsurunaki.novappro.test.UserTable;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -16,7 +15,7 @@ import java.util.Map;
 @WebServlet(name = "ServletLogin", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
 
-    static final By CURRENT_LOGIN_MODE = By.SESSION;
+    private static final By CURRENT_LOGIN_MODE = By.SESSION;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -26,12 +25,6 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         login(CURRENT_LOGIN_MODE, request, response);
-    }
-
-    enum By {
-        SESSION,
-        COOKIE,
-        URL
     }
 
     void login(By by, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -53,11 +46,6 @@ public class LoginServlet extends HttpServlet {
     void loginBySession(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 测试用例: http://localhost:8080/Novappro_war_exploded/login?username=1&rawPassword=12345678
 
-        // 在没有数据库的情况下使用, 之后将不会使用这段代码
-        UserTable.init();
-
-        request.getRemoteAddr();
-
         Integer userId = Integer.valueOf(request.getParameter("userId"));
         String rawPassword = request.getParameter("rawPassword");
 
@@ -65,14 +53,11 @@ public class LoginServlet extends HttpServlet {
             return;
         }
 
-        Map<String, Object> loginResult = LoginService.getInstance().tryLoginWithUserId(userId, rawPassword);
+        Pair<LoginService.VerifyCode, User> verifyCodeUserPair = LoginService.getInstance().tryLoginWithUserId(userId, rawPassword);
+        LoginService.VerifyCode verifyCode = verifyCodeUserPair.getLeft();
+        User user = verifyCodeUserPair.getRight();
 
-        final String VERIFY_CODE = VerifyCode.class.getName();
-        final String USER = User.class.getName();
-
-        if (loginResult.get(VERIFY_CODE) == VerifyCode.OK) {
-
-            User user = (User) loginResult.get(USER);
+        if (verifyCode == LoginService.VerifyCode.OK) {
 
             request.getSession().setAttribute("user_username", user.getUsername());
             request.getSession().setAttribute("user_type", user.getType().getChineseName());
@@ -86,9 +71,6 @@ public class LoginServlet extends HttpServlet {
     void loginByCookie(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 测试用例: http://localhost:8080/Novappro_war_exploded/login?username=1&rawPassword=12345678
 
-        // 在没有数据库的情况下使用, 之后将不会使用这段代码
-        UserTable.init();
-
         Integer userId = Integer.valueOf(request.getParameter("userId"));
         String rawPassword = request.getParameter("rawPassword");
 
@@ -96,13 +78,11 @@ public class LoginServlet extends HttpServlet {
             return;
         }
 
-        Map<String, Object> loginResult = LoginService.getInstance().tryLoginWithUserId(userId, rawPassword);
+        Pair<LoginService.VerifyCode, User> verifyCodeUserPair = LoginService.getInstance().tryLoginWithUserId(userId, rawPassword);
+        LoginService.VerifyCode verifyCode = verifyCodeUserPair.getLeft();
+        User user = verifyCodeUserPair.getRight();
 
-        final String VERIFY_CODE = VerifyCode.class.getName();
-        final String USER = User.class.getName();
-
-        if (loginResult.get(VERIFY_CODE) == VerifyCode.OK) {
-            User user = (User) loginResult.get(USER);
+        if (verifyCode == LoginService.VerifyCode.OK) {
 
             List<Cookie> cookies = new ArrayList<>();
             cookies.add(new Cookie("user_username", user.getUsername()));
@@ -118,9 +98,6 @@ public class LoginServlet extends HttpServlet {
     void loginByURL(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 测试用例: http://localhost:8080/Novappro_war_exploded/login?userId=1&rawPassword=12345678
 
-        // 在没有数据库的情况下使用, 之后将不会使用这段代码
-        UserTable.init();
-
         Integer userId = Integer.valueOf(request.getParameter("userId"));
         String rawPassword = request.getParameter("rawPassword");
 
@@ -128,18 +105,21 @@ public class LoginServlet extends HttpServlet {
             return;
         }
 
-        Map<String, Object> loginResult = LoginService.getInstance().tryLoginWithUserId(userId, rawPassword);
+        Pair<LoginService.VerifyCode, User> verifyCodeUserPair = LoginService.getInstance().tryLoginWithUserId(userId, rawPassword);
+        LoginService.VerifyCode verifyCode = verifyCodeUserPair.getLeft();
+        User user = verifyCodeUserPair.getRight();
 
-        final String VERIFY_CODE = VerifyCode.class.getName();
-        final String USER = User.class.getName();
-
-        if (loginResult.get(VERIFY_CODE) == VerifyCode.OK) {
-            request.setAttribute(USER, loginResult.get(USER));
+        if (verifyCode == LoginService.VerifyCode.OK) {
             request.getRequestDispatcher("welcome.jsp").forward(request, response);
         } else {
             request.getRequestDispatcher("error.jsp").forward(request, response);
         }
     }
 
+    enum By {
+        SESSION,
+        COOKIE,
+        URL
+    }
 
 }
