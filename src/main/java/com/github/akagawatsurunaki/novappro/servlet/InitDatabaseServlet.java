ec02c1a6a174b451124b3aec191dbd5593d5160e
AkagawaTsurunaki <965145619@qq.com>
2023-03-29 17:13:17 +0800

    refactor: 修改类名

--- src/main/java/com/github/akagawatsurunaki/novappro/servlet/InitDatabaseServlet.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/InitDatabaseServlet.java
@@ -1,18 +1,39 @@
-package com.github.akagawatsurunaki.novappro.init;
+package com.github.akagawatsurunaki.novappro.servlet;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.github.akagawatsurunaki.novappro.annotation.Database;
 import com.github.akagawatsurunaki.novappro.config.DatabaseConfig;
+import com.github.akagawatsurunaki.novappro.config.InitSequenceConfig;
+import com.github.akagawatsurunaki.novappro.mapper.impl.UserMapperImpl;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Set;
 
-public class DatabaseManager {
+@WebServlet(name = "InitDatabaseServlet",urlPatterns = {"/initDatabase"}, loadOnStartup = InitSequenceConfig.INIT_DATABASE)
+public class InitDatabaseServlet extends HttpServlet {
+
+    private final static DatabaseManager DATABASE_MANAGER = new DatabaseManager();
+
+    @Override
+    public void init() throws ServletException {
+        // 测试用例: http://localhost:8080/Novappro_war_exploded/InitServlet
+
+        // 初始化数据库
+        DATABASE_MANAGER.init();
+        String s = JSONObject.toJSONString(UserMapperImpl.getInstance().getUserById(1));
+        System.out.println(s);
+    }
+}
+class DatabaseManager {
 
     private Connection connection;
 
