27b7dc601032598a33f882def08a774d1690b50a
AkagawaTsurunaki <965145619@qq.com>
2023-03-29 15:30:36 +0800

    refactor: 修改数据库初始化逻辑, 交由DatabaseManager管理

--- src/main/java/com/github/akagawatsurunaki/novappro/servlet/InitServlet.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/InitServlet.java
@@ -1,7 +1,7 @@
 package com.github.akagawatsurunaki.novappro.servlet;
 
 import com.alibaba.fastjson2.JSONObject;
-import com.github.akagawatsurunaki.novappro.config.DatabaseConfig;
+import com.github.akagawatsurunaki.novappro.init.DatabaseManager;
 import com.github.akagawatsurunaki.novappro.mapper.UserMapper;
 import com.github.akagawatsurunaki.novappro.mapper.impl.UserMapperImpl;
 
@@ -9,18 +9,20 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 @WebServlet(name = "InitServlet", value = "/InitServlet")
 public class InitServlet extends HttpServlet {
+
+    private final static DatabaseManager DATABASE_MANAGER = new DatabaseManager();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 测试用例: http://localhost:8080/Novappro_war_exploded/InitServlet
-        Connection connection = initDatabase();
-        UserMapperImpl.getInstance().setConnection(connection);
-        String s = JSONObject.toJSONString(UserMapperImpl.getInstance().getUser(UserMapper.By.ID, 1));
+
+        // 初始化数据库
+        DATABASE_MANAGER.init();
+        String s = JSONObject.toJSONString(UserMapperImpl.getInstance().getUserById(1));
         System.out.println(s);
     }
 
@@ -29,16 +31,5 @@ public class InitServlet extends HttpServlet {
 
     }
 
-    private Connection initDatabase() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(
-                    DatabaseConfig.getURL(),
-                    DatabaseConfig.getUSER(),
-                    DatabaseConfig.getPASSWORD());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+
 }
