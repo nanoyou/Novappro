ba71163e9eb4438757c63e4179421206301592b2
AkagawaTsurunaki <965145619@qq.com>
2023-04-08 22:45:03 +0800

    feat: 添加注解, 删除无用import语句

--- src/main/java/com/github/akagawatsurunaki/novappro/model/User.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/model/User.java
@@ -1,12 +1,8 @@
 package com.github.akagawatsurunaki.novappro.model;
 
 import com.alibaba.fastjson2.annotation.JSONType;
+import com.github.akagawatsurunaki.novappro.annotation.ChineseFieldName;
 import lombok.*;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @ToString
@@ -14,14 +10,21 @@ import java.util.List;
 @NoArgsConstructor
 @JSONType(ignores = {"rawPassword"})
 public class User {
+
+    @ChineseFieldName(chineseFieldName = "学号/工号")
     Integer id;
+
+    @ChineseFieldName(chineseFieldName = "姓名")
     String username;
+
+    @ChineseFieldName(chineseFieldName = "明文密码")
     String rawPassword;
+
+    @ChineseFieldName(chineseFieldName = "用户类型")
     Type type;
 
     @AllArgsConstructor
     @ToString
-
     public enum Type {
 
         ADMIN(233, "管理员"),
@@ -39,7 +42,7 @@ public class User {
                     return t;
                 }
             }
-            throw new IllegalArgumentException(Type.class.getName() + "错误");
+            throw new IllegalArgumentException(Type.class.getName() + "无法解析的错误");
         }
     }
 
