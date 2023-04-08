144028a573d9a93c00e80c08f2e17335e7075995
AkagawaTsurunaki <965145619@qq.com>
2023-04-08 22:40:30 +0800

    feat: 新增三个全局变量

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/constant/Constant.java
@@ -0,0 +1,9 @@
+package com.github.akagawatsurunaki.novappro.constant;
+
+public class Constant {
+    public static final int MAX_LEN_PASSWORD = 16;
+    public static final int MIN_LEN_PASSWORD = 8;
+
+    public static final int NO_SELECTED_ITEM_ID = -1;
+
+}
