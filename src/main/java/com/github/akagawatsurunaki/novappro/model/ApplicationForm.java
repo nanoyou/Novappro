14f9f1d9c1b2714a1de8ac30605de61628e83575
AkagawaTsurunaki <965145619@qq.com>
2023-04-09 14:28:36 +0800

    feat: 申请表的model

--- src/main/java/com/github/akagawatsurunaki/novappro/model/ApplicationForm.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/model/ApplicationForm.java
@@ -1,7 +1,7 @@
 package com.github.akagawatsurunaki.novappro.model;
 
 import com.github.akagawatsurunaki.novappro.annotation.ChineseFieldName;
-import com.github.akagawatsurunaki.novappro.model.content.Question;
+import com.github.akagawatsurunaki.novappro.model.question.Question;
 
 import java.util.List;
 
@@ -12,6 +12,9 @@ public class ApplicationForm {
 
     @ChineseFieldName(chineseFieldName = "申请表ID")
     Integer id;
+
+    @ChineseFieldName(chineseFieldName = "申请人ID")
+    Integer applicantId;
 
     @ChineseFieldName(chineseFieldName = "问题列表")
     List<Question> questions;
