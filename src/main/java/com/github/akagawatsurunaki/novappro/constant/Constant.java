0194894fcb062284b031d8c853c36a06077ef836
AkagawaTsurunaki <965145619@qq.com>
2023-04-10 23:35:22 +0800

    feat: 增加更多的常量

--- src/main/java/com/github/akagawatsurunaki/novappro/constant/Constant.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/constant/Constant.java
@@ -1,5 +1,9 @@
 package com.github.akagawatsurunaki.novappro.constant;
 
+import lombok.experimental.FieldNameConstants;
+
+import java.math.BigDecimal;
+
 public class Constant {
     public static final int MAX_LEN_PASSWORD = 16;
     public static final int MIN_LEN_PASSWORD = 8;
@@ -14,5 +18,16 @@ public class Constant {
 
     public static final int MIN_LEN_USERNAME = 1;
     public static final int MAX_LEN_USERNAME = 25;
+
+    public static final int MAX_LEN_COURSE_NAME = 127;
+
+    public static final int MAX_LEN_COURSE_SERIAL_NUMBER = 7;
+    public static final int MAX_LEN_COURSE_ONLINE_CONTACT_WAY = 255;
+    public static final int MAX_LEN_COURSE_COMMENT = 511;
+
+
+    public static final BigDecimal MAX_LEN_COURSE_CREDIT = new BigDecimal("10.00");
+
+
 
 }
