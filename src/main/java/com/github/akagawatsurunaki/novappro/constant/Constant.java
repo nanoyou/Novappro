297f8823a00f66aaa1674282472f2517cc244a09
AkagawaTsurunaki <965145619@qq.com>
2023-04-10 18:11:08 +0800

    feat: UserId和Username的约束常量

--- src/main/java/com/github/akagawatsurunaki/novappro/constant/Constant.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/constant/Constant.java
@@ -4,6 +4,15 @@ public class Constant {
     public static final int MAX_LEN_PASSWORD = 16;
     public static final int MIN_LEN_PASSWORD = 8;
 
+    public static final int MAX_LEN_USER_ID = 10;
+
+    public static final int MIN_LEN_USER_ID = 8;
+
     public static final int NO_SELECTED_ITEM_ID = -1;
+
+    public static final int DEFAULT_APPLICATION_FORM_ID = -1;
+
+    public static final int MIN_LEN_USERNAME = 1;
+    public static final int MAX_LEN_USERNAME = 25;
 
 }
