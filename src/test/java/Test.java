4ab0c186e46fc1d52ba20e424d63701a81d3ac07
AkagawaTsurunaki <965145619@qq.com>
2023-04-10 23:31:53 +0800

    test: 测试selectAllCourses方法

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,11 @@
+import cn.hutool.core.util.StrUtil;
+import com.github.akagawatsurunaki.novappro.mapper.impl.CourseMapperImpl;
+import com.github.akagawatsurunaki.novappro.model.course.Course;
+
 public class Test {
     public static void main(String[] args) {
-
+        String s = StrUtil.toUnderlineCase(Course.class.getSimpleName());
+        System.out.println(s);
+        CourseMapperImpl.getInstance().selectAllCourses();
     }
 }
