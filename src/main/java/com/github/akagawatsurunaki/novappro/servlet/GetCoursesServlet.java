e59f47c3362b8005f2d31324612fd56f2a1f9337
AkagawaTsurunaki <965145619@qq.com>
2023-04-11 13:37:57 +0800

    feat: 通过请求转发完成课表显示功能

--- src/main/java/com/github/akagawatsurunaki/novappro/servlet/GetCoursesServlet.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/GetCoursesServlet.java
@@ -1,5 +1,6 @@
 package com.github.akagawatsurunaki.novappro.servlet;
 
+import com.github.akagawatsurunaki.novappro.constant.ServletConstant;
 import com.github.akagawatsurunaki.novappro.model.course.Course;
 import com.github.akagawatsurunaki.novappro.service.CourseService;
 import org.apache.commons.lang3.tuple.Pair;
@@ -12,22 +13,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static com.github.akagawatsurunaki.novappro.constant.LiteralConstant.COURSES;
 import static com.github.akagawatsurunaki.novappro.constant.ServletConstant.GET_COURSE_SERVLET;
 import static com.github.akagawatsurunaki.novappro.service.CourseService.VerifyCode.*;
 
 @WebServlet(name = "GetCoursesServlet", urlPatterns = GET_COURSE_SERVLET)
 public class GetCoursesServlet extends HttpServlet {
+
+    public static final String JSP_RESOURCE = "courses.jsp";
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Pair<CourseService.VerifyCode, List<Course>> resultPair = CourseService.getInstance().getAllCourses();
         CourseService.VerifyCode verifyCode = resultPair.getLeft();
+        List<Course> courses = resultPair.getRight();
         if (verifyCode == SERVICE_OK) {
-            resultPair.getRight().forEach(System.out::println);
+            request.setAttribute(ServletConstant.INFO, "成功查询到" + courses.size() + "门课程。");
         } else if (verifyCode == SERVICE_OK_BUT_EMPTY_COURSES_LIST) {
-            System.out.println("Empty courses");
+            request.setAttribute(ServletConstant.INFO, "没有查询到课程。");
         } else if (verifyCode == SERVICE_ERROR) {
-            System.out.println("ERROR!!!!");
+            request.setAttribute(ServletConstant.INFO, "查询服务故障，请联系管理员。");
         }
+        request.setAttribute(verifyCode.getClass().getSimpleName(), verifyCode);
+        request.setAttribute(COURSES, courses);
+        request.getRequestDispatcher(JSP_RESOURCE).forward(request, response);
     }
 
     @Override
