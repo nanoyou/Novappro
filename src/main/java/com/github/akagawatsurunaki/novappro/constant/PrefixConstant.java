1b83a7d4da4eac03329b0b00fc14301cb3b72996
AkagawaTsurunaki <965145619@qq.com>
2023-04-12 23:34:36 +0800

    feat: 增加前缀常量

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/constant/PrefixConstant.java
@@ -0,0 +1,8 @@
+package com.github.akagawatsurunaki.novappro.constant;
+
+public class PrefixConstant
+{
+    public static final String APPLICATION_ENTITY_CODE = "aec:";
+    public static final String APPROVAL_PROCESS_NODE_CODE = "apnc:";
+    public static final String APPROVAL_PROCESS_QUEUE_CODE = "apqc:";
+}
