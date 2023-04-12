ec0746a524b3dcfa7c9a7aa851089c44028c10e2
AkagawaTsurunaki <965145619@qq.com>
2023-04-12 23:38:13 +0800

    feat: 通过课程代码获取课程方法

--- src/main/java/com/github/akagawatsurunaki/novappro/service/CourseService.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/service/CourseService.java
@@ -5,9 +5,11 @@ import com.github.akagawatsurunaki.novappro.mapper.impl.CourseMapperImpl;
 import com.github.akagawatsurunaki.novappro.model.course.Course;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -18,10 +20,10 @@ public class CourseService {
 
     private static final CourseMapper COURSE_MAPPER = CourseMapperImpl.getInstance();
 
-    public Pair<VerifyCode, List<Course>> getAllCourses(){
+    public Pair<VerifyCode, List<Course>> getAllCourses() {
 
         Pair<CourseMapper.VerifyCode, List<Course>> verifyCodeListPair = COURSE_MAPPER.selectAllCourses();
-        if(verifyCodeListPair.getLeft() == CourseMapper.VerifyCode.MAPPER_OK){
+        if (verifyCodeListPair.getLeft() == CourseMapper.VerifyCode.SQL_OK) {
             List<Course> courses = verifyCodeListPair.getRight();
             if (courses.isEmpty()) {
                 return new ImmutablePair<>(VerifyCode.SERVICE_OK_BUT_EMPTY_COURSES_LIST, courses);
@@ -31,10 +33,37 @@ public class CourseService {
         return new ImmutablePair<>(VerifyCode.SERVICE_ERROR, null);
     }
 
-    public enum VerifyCode{
+    public Pair<VerifyCode, List<Course>> getCoursesByCodes(@NonNull List<String> codes) {
+
+        List<Course> courses = new ArrayList<>();
+        boolean noSuchCourse = false;
+
+        for (String code : codes) {
+            CourseMapper.VerifyCode verifyCode = COURSE_MAPPER.selectCourseByCode(code).getLeft();
+            Course course = COURSE_MAPPER.selectCourseByCode(code).getRight();
+
+            if (verifyCode == CourseMapper.VerifyCode.SQL_OK) {
+                courses.add(course);
+            } else if (verifyCode == CourseMapper.VerifyCode.NO_SUCH_COURSE) {
+                noSuchCourse = true;
+            } else if (verifyCode == CourseMapper.VerifyCode.FAILED_TO_SELECT) {
+                return new ImmutablePair<>(VerifyCode.SERVICE_ERROR, null);
+            }
+        }
+
+        // 部分用户可能没有被返回
+        if (noSuchCourse){
+            return new ImmutablePair<>(VerifyCode.CAN_NOT_FIND_SOME_ENTITIES, courses);
+        }
+
+        return new ImmutablePair<>(VerifyCode.SERVICE_OK, courses);
+    }
+
+    public enum VerifyCode {
         SERVICE_OK,
         SERVICE_OK_BUT_EMPTY_COURSES_LIST,
         SERVICE_ERROR,
+        CAN_NOT_FIND_SOME_ENTITIES,
     }
 
 }
