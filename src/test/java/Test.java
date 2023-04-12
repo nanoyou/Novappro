d442f529e928f54b57f8cd96d584b17e6d8dc332
AkagawaTsurunaki <965145619@qq.com>
2023-04-12 23:36:43 +0800

    test: 测试CourseApplication

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,13 @@
 import cn.hutool.core.util.StrUtil;
 import com.github.akagawatsurunaki.novappro.mapper.impl.CourseMapperImpl;
+import com.github.akagawatsurunaki.novappro.model.approval.CourseApplication;
 import com.github.akagawatsurunaki.novappro.model.course.Course;
+
+import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        String s = StrUtil.toUnderlineCase(Course.class.getSimpleName());
-        System.out.println(s);
-        CourseMapperImpl.getInstance().selectAllCourses();
+        CourseApplication courseApplication = new CourseApplication();
+        courseApplication.setAppliedCourses(new ArrayList<>());
     }
 }
