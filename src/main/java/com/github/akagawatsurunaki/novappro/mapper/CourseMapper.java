ae6a1dee69e0ba8b6466cacca9315b3a6dd5367c
AkagawaTsurunaki <965145619@qq.com>
2023-04-12 23:32:20 +0800

    feat: 新增查询通过课程码查询课程的功能

--- src/main/java/com/github/akagawatsurunaki/novappro/mapper/CourseMapper.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/mapper/CourseMapper.java
@@ -1,6 +1,7 @@
 package com.github.akagawatsurunaki.novappro.mapper;
 
 import com.github.akagawatsurunaki.novappro.model.course.Course;
+import lombok.NonNull;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
@@ -9,8 +10,11 @@ public interface CourseMapper {
 
     Pair<VerifyCode, List<Course>> selectAllCourses();
 
+    Pair<VerifyCode, Course> selectCourseByCode(@NonNull String code);
+
     enum VerifyCode{
-        MAPPER_OK,
+        SQL_OK,
         FAILED_TO_SELECT,
+        NO_SUCH_COURSE,
     }
 }
--- src/main/java/com/github/akagawatsurunaki/novappro/mapper/impl/CourseMapperImpl.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/mapper/impl/CourseMapperImpl.java
@@ -8,13 +8,13 @@ import cn.hutool.db.Entity;
 import com.github.akagawatsurunaki.novappro.mapper.CourseMapper;
 import com.github.akagawatsurunaki.novappro.model.course.Course;
 import lombok.Getter;
+import lombok.NonNull;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.math.BigDecimal;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,10 +28,6 @@ public class CourseMapperImpl implements CourseMapper {
     @Getter
     private static  final  UserMapperImpl USER_MAPPER = UserMapperImpl.getInstance();
 
-    public static List<Course> parseResultSet(ResultSet rs) {
-        return null;
-    }
-
     String selectAllCoursesSQL = "mysql/select_all_courses.sql";
     @Override
     public Pair<VerifyCode, List<Course>> selectAllCourses() {
@@ -42,26 +38,49 @@ public class CourseMapperImpl implements CourseMapper {
 
             List<Course> courses = new ArrayList<>();
             courseEntities.forEach(entity -> {
-                Course course = new Course();
-                course.setCode((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.code)));
-                course.setName((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.name)));
-                course.setCredit((BigDecimal) entity.get(StrUtil.toUnderlineCase(Course.Fields.credit)));
-                course.setSerialNumber(((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.serialNumber))));
-                course.setOnlineContactWay((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.onlineContactWay)));
-                course.setComment((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.comment)));
-
-                course.setTeachers(
-                        Arrays.stream(
-                                ((String) entity.get(StrUtil.toUnderlineCase(
-                                        Course.Fields.teachers))).split(", ")).toList());
-
+                Course course = parseCourseEntity(entity);
                 courses.add(course);
             });
 
-            return new ImmutablePair<>(VerifyCode.MAPPER_OK, courses);
+            return new ImmutablePair<>(VerifyCode.SQL_OK, courses);
         } catch (SQLException e) {
             e.printStackTrace();
             return new ImmutablePair<>(VerifyCode.FAILED_TO_SELECT, null);
         }
     }
+
+    String selectCourseByCodeSQL = "mysql/select_course_by_code.sql";
+    @Override
+    public Pair<VerifyCode, Course> selectCourseByCode(@NonNull String code) {
+
+        try {
+            URL resource = ResourceUtil.getResource(selectCourseByCodeSQL);
+            List<Entity> courseEntities = Db.use().query(FileUtil.readString(resource, StandardCharsets.UTF_8), code);
+
+            if (courseEntities == null){
+                return new ImmutablePair<>(VerifyCode.NO_SUCH_COURSE, null);
+            }
+            Course course = parseCourseEntity(courseEntities.get(0));
+            return new ImmutablePair<>(VerifyCode.SQL_OK, course);
+        } catch (SQLException e) {
+            return new ImmutablePair<>(VerifyCode.FAILED_TO_SELECT, null);
+        }
+    }
+
+    private Course parseCourseEntity(Entity entity) {
+        Course course = new Course();
+        course.setCode((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.code)));
+        course.setName((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.name)));
+        course.setCredit((BigDecimal) entity.get(StrUtil.toUnderlineCase(Course.Fields.credit)));
+        course.setSerialNumber(((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.serialNumber))));
+        course.setOnlineContactWay((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.onlineContactWay)));
+        course.setComment((String) entity.get(StrUtil.toUnderlineCase(Course.Fields.comment)));
+        course.setTeachers(
+                Arrays.stream(
+                        ((String) entity.get(StrUtil.toUnderlineCase(
+                                Course.Fields.teachers))).split(", ")).toList());
+        return course;
+    }
+
+
 }
