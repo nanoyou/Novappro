66b1421ed99a6d5de29606fc268091c4910c13f1
AkagawaTsurunaki <965145619@qq.com>
2023-04-16 19:44:34 +0800

    feat: CourseUtil.java 实现了将数据库里的courseIdsStr字段解析成字符串列表

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/util/CourseUtil.java
@@ -0,0 +1,14 @@
+package com.github.akagawatsurunaki.novappro.util;
+
+import lombok.NonNull;
+
+import java.util.List;
+
+public final class CourseUtil {
+
+    public static List<String> getCourseCodes(@NonNull String courseIdsStr) {
+        return List.of(courseIdsStr);
+    }
+
+
+}
