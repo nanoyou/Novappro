483482cdf9cde92966e4b6bf9c4bf09850ff63d6
AkagawaTsurunaki <965145619@qq.com>
2023-04-16 11:26:09 +0800

    test: 自动从Entity转化为对应的对象

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,7 @@ import cn.hutool.core.util.EnumUtil;
 import cn.hutool.db.Db;
 import cn.hutool.db.Entity;
 import com.github.akagawatsurunaki.novappro.annotation.Table;
+import com.github.akagawatsurunaki.novappro.constant.VerifyCode;
 import com.github.akagawatsurunaki.novappro.enumeration.ApprovalStatus;
 import com.github.akagawatsurunaki.novappro.enumeration.BusType;
 import com.github.akagawatsurunaki.novappro.mapper.ApprovalFlowDetailMapper;
@@ -10,12 +11,13 @@ import com.github.akagawatsurunaki.novappro.mapper.impl.ApprovalFlowDetailMapper
 import com.github.akagawatsurunaki.novappro.mapper.impl.ApprovalFlowMapperImpl;
 import com.github.akagawatsurunaki.novappro.model.approval.ApprovalFlow;
 import com.github.akagawatsurunaki.novappro.model.approval.ApprovalFlowDetail;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Date;
 
 public class Test {
     public static void main(String[] args){
-        func2();
+        func3();
     }
 
     static void func1() {
@@ -28,6 +30,13 @@ public class Test {
         ApprovalFlowDetailMapper approvalFlowDetailMapper = ApprovalFlowDetailMapperImpl.getInstance();
         var s = new ApprovalFlowDetail(111, "234", 12, "sdff", new Date(), ApprovalStatus.AUDITING);
         approvalFlowDetailMapper.insert(s);
+    }
+
+    static void func3() {
+        ApprovalFlowDetailMapper approvalFlowDetailMapper = ApprovalFlowDetailMapperImpl.getInstance();
+        Pair<VerifyCode.Mapper, ApprovalFlowDetail> pair = approvalFlowDetailMapper.select(111);
+        ApprovalFlowDetail approvalFlowDetail = pair.getRight();
+        System.out.println("approvalFlowDetail = " + approvalFlowDetail);
     }
 
 }
