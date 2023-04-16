e545eb727fe894bfb0d90330b19550a0a7c15ed2
AkagawaTsurunaki <965145619@qq.com>
2023-04-16 19:47:14 +0800

    feat: Servlet层面的常量管理

--- src/main/java/com/github/akagawatsurunaki/novappro/constant/ServletConstant.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/constant/ServletConstant.java
@@ -1,7 +1,18 @@
 package com.github.akagawatsurunaki.novappro.constant;
 
-public final class ServletConstant {
+import lombok.AllArgsConstructor;
+
+public class ServletConstant {
     public static final String GET_COURSE_SERVLET = "/get_courses";
 
     public static final String INFO = "info";
+
+
+    @AllArgsConstructor
+    public enum RequestAttr{
+        LOGIN_USER_ID("login_user_id"),
+        COURSE_APPLICATIONS("course_applications");
+
+        public final String name;
+    }
 }
