ed2b6a32e238eda24d798ca2f819d0bd34a0d1cb
AkagawaTsurunaki <965145619@qq.com>
2023-04-17 23:11:39 +0800

    feat: 线性审批流程

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/interfase/LinearBus.java
@@ -0,0 +1,8 @@
+package com.github.akagawatsurunaki.novappro.interfase;
+
+import com.github.akagawatsurunaki.novappro.model.approval.ApprovalFlowDetail;
+
+public interface LinearBus {
+
+    ApprovalFlowDetail nextNode();
+}
