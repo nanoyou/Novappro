6f1eecb8bd0ed6ed4eee7941c606bb090c53a482
AkagawaTsurunaki <965145619@qq.com>
2023-04-19 23:53:45 +0800

    feat: frontend will get request attribute with applItems

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/appro/ApproServlet.java
@@ -0,0 +1,52 @@
+package com.github.akagawatsurunaki.novappro.servlet.appro;
+
+import com.github.akagawatsurunaki.novappro.constant.ServletConstant;
+import com.github.akagawatsurunaki.novappro.constant.VerifyCode;
+import com.github.akagawatsurunaki.novappro.service.appro.ApprovalService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ApproServlet", value = ServletConstant.WebServletValue.GET_APPROS)
+public class ApproServlet extends HttpServlet {
+
+    private static final ApprovalService APPROVAL_SERVICE = ApprovalService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+        request.setCharacterEncoding("UTF-8");
+
+        var loginUserIdStr = (String) request.getSession().getAttribute(ServletConstant.RequestAttr.LOGIN_USER_ID.name);
+
+        // 如果用户已经登录
+        if (loginUserIdStr != null) {
+
+            var loginUserId = Integer.valueOf(loginUserIdStr);
+
+            // 获取该用户名下的所有ApplItem s.
+
+            var vc_applItems = APPROVAL_SERVICE.getApplItems(loginUserId);
+
+            if (vc_applItems.getLeft() == VerifyCode.Service.OK) {
+
+                var applItems = vc_applItems.getRight();
+                request.setAttribute(ServletConstant.RequestAttr.APPL_ITEMS_WITH_GIVEN_APPROVER.name, applItems);
+
+            }
+
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+    }
+}
