a61e1547474071434b1843fbbbf039686087e444
AkagawaTsurunaki <965145619@qq.com>
2023-04-29 23:26:01 +0800

    feat: 增加了新的枚举成员

--- src/main/java/com/github/akagawatsurunaki/novappro/enumeration/ApprovalStatus.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/enumeration/ApprovalStatus.java
@@ -9,7 +9,9 @@ public enum ApprovalStatus {
     LECTURE_TEACHER_EXAMING("课程主讲教师审批中"),
     SUPERVISOR_TEACHER_EXAMING("课程主管教师审批中"),
     APPROVED("审批成功"),
-    REJECTED("审批驳回");
+    REJECTED("审批驳回"),
+    FINISHED("审批完成")
+    ;
 
     public final String chinese;
 }
