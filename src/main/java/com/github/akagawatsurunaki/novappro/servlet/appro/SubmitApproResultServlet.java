c39a3fe436ead6997612fe75193d151f478b0ccf
AkagawaTsurunaki <965145619@qq.com>
2023-04-29 23:33:11 +0800

    feat: 创建了一个尚未实现的类

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/appro/SubmitApproResultServlet.java
@@ -0,0 +1,17 @@
+package com.github.akagawatsurunaki.novappro.servlet.appro;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "SubmitApproResultServlet", value = "/submit_appro_ret")
+public class SubmitApproResultServlet extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+    }
+}
