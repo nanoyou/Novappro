214dfd33096e71ce2aab5e8901bd9f13c73e8d97
AkagawaTsurunaki <965145619@qq.com>
2023-04-30 18:37:32 +0800

    feat: 提交审批(Appro)结果, 持久化保存 功能的Servlet实现

--- src/main/java/com/github/akagawatsurunaki/novappro/servlet/appro/SubmitApproResultServlet.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/appro/SubmitApproResultServlet.java
@@ -1,10 +1,18 @@
 package com.github.akagawatsurunaki.novappro.servlet.appro;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.github.akagawatsurunaki.novappro.constant.SC;
+import com.github.akagawatsurunaki.novappro.service.appro.ApprovalService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 提交审批(Appro)结果, 持久化保存
+ */
 @WebServlet(name = "SubmitApproResultServlet", value = "/submit_appro_ret")
 public class SubmitApproResultServlet extends HttpServlet {
 
@@ -12,6 +20,29 @@ public class SubmitApproResultServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        String applRemark = request.getParameter(SC.ReqParam.APPL_REMARK.name);
+        String applItemConfirm = request.getParameter(SC.ReqParam.APPL_ITEM_CONFIRM.name);
+        String flowNo = request.getParameter(SC.ReqParam.SELECTED_APPL_ITEM_FLOW_NO.name);
+
+        if (applItemConfirm == null){
+            return;
+        }
+
+        if (applItemConfirm.isBlank()){
+            return;
+        }
+
+        if (applRemark == null){
+            applRemark = "";
+        }
+
+        if ("同意审批".equals(applItemConfirm)) {
+            ApprovalService.getInstance().saveApproResult(flowNo, applRemark, true);
+        } else if ("驳回审批".equals(applItemConfirm)) {
+            ApprovalService.getInstance().saveApproResult(flowNo, applRemark, false);
+        }
 
     }
 }
