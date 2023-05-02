b04724321e35e0bd8c135a204ad4412fbd47e3e6
AkagawaTsurunaki <965145619@qq.com>
2023-05-02 15:31:04 +0800

    feat: 文件存储位置配置文件

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/config/ResourceConfig.java
@@ -0,0 +1,5 @@
+package com.github.akagawatsurunaki.novappro.config;
+
+public class ResourceConfig {
+    public static final String UPLOADED_IMG_PATH = "D:/WorkSpace/Project/WebDev/Novappro/data/upload/img";
+}
