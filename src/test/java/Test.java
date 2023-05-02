ba8c7307d48a2601749e9c5de0d438fcd1339974
AkagawaTsurunaki <965145619@qq.com>
2023-05-02 18:16:06 +0800

    refactor: 删除无用的代码

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -17,45 +17,4 @@ public class Test {
 
     }
 
-    static void func1() {
-        ApprovalFlowMapper approvalFlowMapper = ApprovalFlowMapperImpl.getInstance();
-        var af = new ApprovalFlow("1231234", ApprovalStatus.APPROVED, "asdf", BusType.LINEAR, 123, new Date());
-        approvalFlowMapper.insert(af);
-    }
-
-    static void func2() {
-        ApprovalFlowDetailMapper approvalFlowDetailMapper = ApprovalFlowDetailMapperImpl.getInstance();
-        var s = new ApprovalFlowDetail(111, "234", 12, "sdff", new Date(), ApprovalStatus.SUBMITTED);
-        approvalFlowDetailMapper.insert(s);
-    }
-
-//    static void func3() {
-//        ApprovalFlowDetailMapper approvalFlowDetailMapper = ApprovalFlowDetailMapperImpl.getInstance();
-//        Pair<VC.Mapper, ApprovalFlowDetail> pair = approvalFlowDetailMapper.select(111);
-//        ApprovalFlowDetail approvalFlowDetail = pair.getRight();
-//        System.out.println("approvalFlowDetail = " + approvalFlowDetail);
-//    }
-
-    static void func4() {
-        List<String> strs = new ArrayList<>();
-        strs.add("1111");
-        strs.add("2222");
-        List<String> courseCodes = CourseUtil.getCourseCodes(strs.toString());
-    }
-//    static void func5(){
-//        ApplItem applItem = new ApplItem();
-//        var s = ZhFieldUtil.getZhValue(ApplItem.class, ApplItem.Fields.applicantName);
-//        System.out.println(s);
-//    }
-//
-//    static void func6() {
-//        var s = ApprovalService.getInstance().getApplItems("APFL2021000216818008100493182");
-//        System.out.println(s);
-//    }
-
-    static void func7() {
-
-
-    }
-
 }
