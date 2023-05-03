ea57af87fc9cd9a20ba3eaf6f392fe200cb8d3cd
AkagawaTsurunaki <965145619@qq.com>
2023-05-03 19:52:23 +0800

    test: 测试通过MyDb

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/init/InitServlet.java
@@ -0,0 +1,31 @@
+package com.github.akagawatsurunaki.novappro.servlet.init;
+
+import com.github.akagawatsurunaki.novappro.mapper.UserMapper;
+import com.github.akagawatsurunaki.novappro.util.MyDb;
+import org.apache.ibatis.session.SqlSession;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "InitServlet", value = "/InitServlet")
+public class InitServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        MyDb.init();
+
+        try(SqlSession session = MyDb.use().openSession()){
+            var mapper = session.getMapper(UserMapper.class);
+            var user = mapper.selectById(20210000);
+            System.out.println("user = " + user);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+    }
+}
---
+++ src/main/java/com/github/akagawatsurunaki/novappro/util/MyDb.java
@@ -0,0 +1,30 @@
+package com.github.akagawatsurunaki.novappro.util;
+
+import cn.hutool.core.io.resource.ResourceUtil;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyDb {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    private static boolean isInited = false;
+
+    public static SqlSessionFactory use() throws IOException {
+        if (!isInited){
+            init();
+        }
+        return sqlSessionFactory;
+    }
+
+    public static void init() throws IOException {
+        var resource = ResourceUtil.getResource("config/mybatis/mybatis-config.xml");
+        InputStream inputStream = Resources.getResourceAsStream("config/mybatis/mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        isInited = true;
+    }
+}
