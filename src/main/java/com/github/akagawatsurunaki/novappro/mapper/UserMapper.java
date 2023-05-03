21b074ecb8d8f8330e65f60915ac378df495cabc
AkagawaTsurunaki <965145619@qq.com>
2023-05-03 21:38:58 +0800

    feat: 完全实现UserMapper的MyBatis映射

--- src/main/java/com/github/akagawatsurunaki/novappro/mapper/UserMapper.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/mapper/UserMapper.java
@@ -1,9 +1,6 @@
 package com.github.akagawatsurunaki.novappro.mapper;
 
-import com.github.akagawatsurunaki.novappro.constant.VC;
 import com.github.akagawatsurunaki.novappro.model.database.User;
-import lombok.NonNull;
-import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.List;
 
@@ -11,8 +8,8 @@ public interface UserMapper {
 
     User selectById(int id);
 
-    Pair<VC.Mapper, User> selectUserById(@NonNull Integer id);
-    Pair<VC.Mapper, List<User>> selectAllUsers();
-    Pair<VC.Mapper, User> insertUser(User user);
+    List<User> selectAllUsers();
+
+    int insert(User user);
 
 }
