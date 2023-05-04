8ba2aab2e0be858673ceb5691560201c52d1a7fb
AkagawaTsurunaki <965145619@qq.com>
2023-05-04 23:51:33 +0800

    feat: CourseApplicationMapper 使用 Mybatis 映射

--- src/main/java/com/github/akagawatsurunaki/novappro/mapper/CourseApplicationMapper.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/mapper/CourseApplicationMapper.java
@@ -9,14 +9,13 @@ import java.util.List;
 
 public interface CourseApplicationMapper {
 
-    Pair<VC.Mapper, CourseApplication> insert(@NonNull CourseApplication courseApplication);
-    Pair<VC.Mapper, List<CourseApplication>> select(@NonNull Integer userId);
+    int insert(@NonNull CourseApplication courseApplication);
+    List<CourseApplication> select(@NonNull Integer addUserId);
 
-    Pair<VC.Mapper, CourseApplication> selectByFlowNo(@NonNull String flowNo);
+    CourseApplication selectByFlowNo(@NonNull String flowNo);
 
-    Pair<VC.Mapper, CourseApplication> update(@NonNull CourseApplication courseApplication);
-    Pair<VC.Mapper, CourseApplication> update(@NonNull List<CourseApplication> courseApplications);
+    CourseApplication update(@NonNull CourseApplication courseApplications);
 
-    Pair<VC.Mapper, CourseApplication> delete(@NonNull CourseApplication courseApplication);
+  //  Pair<VC.Mapper, CourseApplication> delete(@NonNull CourseApplication courseApplication);
 
 }
