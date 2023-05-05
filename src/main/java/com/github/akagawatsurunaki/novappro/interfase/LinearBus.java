070f7fd5528a06e1bcec369a1d6431833396c116
AkagawaTsurunaki <965145619@qq.com>
2023-05-05 09:01:19 +0800

    refactor: delete a useless java class LinearBus.java because the approval schema has been changed.

--- src/main/java/com/github/akagawatsurunaki/novappro/interfase/LinearBus.java
+++
@@ -1,8 +0,0 @@
-package com.github.akagawatsurunaki.novappro.interfase;
-
-import com.github.akagawatsurunaki.novappro.model.database.approval.ApprovalFlowDetail;
-
-public interface LinearBus {
-
-    ApprovalFlowDetail nextNode();
-}
