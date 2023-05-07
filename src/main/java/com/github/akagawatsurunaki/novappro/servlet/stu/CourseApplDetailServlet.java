0f9c2b7954ab8005e1e23e34cb6849e22a61382e
AkagawaTsurunaki <965145619@qq.com>
2023-05-07 21:04:07 +0800

    refactor: 将统一管理的ReqAttr进行拆分

--- src/main/java/com/github/akagawatsurunaki/novappro/servlet/stu/CourseApplDetailServlet.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/servlet/stu/CourseApplDetailServlet.java
@@ -2,6 +2,7 @@ package com.github.akagawatsurunaki.novappro.servlet.stu;
 
 import com.github.akagawatsurunaki.novappro.constant.SC;
 import com.github.akagawatsurunaki.novappro.service.stu.CourseApplDetailService;
+import lombok.AllArgsConstructor;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -14,16 +15,25 @@ import java.io.IOException;
 @WebServlet(name = "CourseApplDetailServlet", value = SC.WebServletValue.COURSE_APPL_DETAIL)
 public class CourseApplDetailServlet extends HttpServlet {
 
-    private final static CourseApplDetailService instance = new CourseApplDetailService();
+    private final static CourseApplDetailService COURSE_APPL_DETAIL_SERVICE = new CourseApplDetailService();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
+        // 获取流水号
         var flowCode = request.getParameter(SC.ReqParam.SELECTED_COURSE_APPL_FLOW_NO.name);
         // 获取该流水号下的申请的课程 (可能有多个)
-        var courses = instance.getAppliedCourses(flowCode);
-        request.setAttribute(SC.ReqAttr.SELECTED_COURSE_APPL_FLOW_NO.name, flowCode);
-        request.setAttribute(SC.ReqAttr.APPLIED_COURSES.name, courses);
+        var getAppliedCoursesServiceResult = COURSE_APPL_DETAIL_SERVICE.getAppliedCourses(flowCode);
+        request.setAttribute(ReqAttr.SELECTED_COURSE_APPL_FLOW_NO.value, flowCode);
+        request.setAttribute(ReqAttr.GET_APPLIED_COURSES_SERVICE_RESULT.value, getAppliedCoursesServiceResult);
         request.getRequestDispatcher(SC.JSPResource.COURSE_APPL_DETAIL.name).forward(request, response);
+    }
+
+    @AllArgsConstructor
+    public enum ReqAttr {
+        GET_APPLIED_COURSES_SERVICE_RESULT("get_applied_courses_service_result"),
+        SELECTED_COURSE_APPL_FLOW_NO("selected_course_appl_flow_no");
+
+        public final String value;
     }
 }
