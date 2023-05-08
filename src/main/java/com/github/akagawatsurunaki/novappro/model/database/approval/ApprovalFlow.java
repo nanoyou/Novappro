c96f420355a27c87c4038e826cc76db796dc918e
AkagawaTsurunaki <965145619@qq.com>
2023-05-08 15:20:05 +0800

    feat: add ZhField annotation to each field in ApprovalFlow

--- src/main/java/com/github/akagawatsurunaki/novappro/model/database/approval/ApprovalFlow.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/model/database/approval/ApprovalFlow.java
@@ -2,6 +2,7 @@ package com.github.akagawatsurunaki.novappro.model.database.approval;
 
 import com.github.akagawatsurunaki.novappro.annotation.Field;
 import com.github.akagawatsurunaki.novappro.annotation.Table;
+import com.github.akagawatsurunaki.novappro.annotation.ZhField;
 import com.github.akagawatsurunaki.novappro.enumeration.ApprovalStatus;
 import com.github.akagawatsurunaki.novappro.enumeration.BusType;
 import lombok.*;
@@ -27,6 +28,7 @@ public class ApprovalFlow {
     /**
      * 审批流ApprovalFlow的唯一标志号码, 又名流水号
      */
+    @ZhField("流水号")
     @Field("flow_no")
     String flowNo;
 
@@ -35,12 +37,14 @@ public class ApprovalFlow {
      *
      * @implNote 如果实现了LinearBus审批流程模式, 那么审批流ApprovalFlow的状态应与最后一个审批明细的状态一致
      */
+    @ZhField("当前审批状态")
     @Field("appro_status")
     ApprovalStatus approStatus;
 
     /**
      * 审批流ApprovalFlow的标题
      */
+    @ZhField("标题")
     @Field("title")
     String title;
 
@@ -49,24 +53,28 @@ public class ApprovalFlow {
      *
      * @implNote LinearBus将审批流程组成一条直线, 即是一个线性流程图.
      */
+    @ZhField("审批流拓扑结构")
     @Field("bus_type")
     BusType busType;
 
     /**
      * 审批流ApprovalFlow的创建人ID
      */
+    @ZhField("申请人ID")
     @Field("add_user_id")
     Integer addUserId;
 
     /**
      * 审批流ApprovalFlow的创建时间
      */
+    @ZhField("申请发起时间")
     @Field("add_time")
     Date addTime;
 
     /**
      * 申请人填写的申请原因
      */
+    @ZhField("申请原因")
     @Field("remark")
     String remark;
 
