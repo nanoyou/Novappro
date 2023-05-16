a6b9566a1c342c9bc578e045f692771a931e70bc
AkagawaTsurunaki <965145619@qq.com>
2023-05-16 10:07:40 +0800

    feat: 尝试编写安卓端访问的servlet

---
+++ src/main/java/com/github/akagawatsurunaki/novappro/android/stu/GetAllCoursesServlet.java
@@ -0,0 +1,26 @@
+package com.github.akagawatsurunaki.novappro.android.stu;
+
+import com.alibaba.fastjson2.JSON;
+import com.github.akagawatsurunaki.novappro.service.stu.CourseService;
+import com.github.akagawatsurunaki.novappro.util.ResponseUtil;
+import lombok.val;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "GetAllCoursesServlet", value = "/android/getAllCourses")
+public class GetAllCoursesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        val getAllCoursesServiceResult = CourseService.getInstance().getAllCourses();
+        val jsonString = JSON.toJSONString(getAllCoursesServiceResult);
+        ResponseUtil.setBody(response, jsonString);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
