a11e75ebeb59922f99a293b91443cabaea796373
AkagawaTsurunaki <965145619@qq.com>
2023-05-25 08:58:44 +0800

    refactor: 删除无用实体类

--- src/main/java/com/github/akagawatsurunaki/novappro/android/stu/service/base/LoginServletAndroid.java
+++ src/main/java/com/github/akagawatsurunaki/novappro/android/stu/service/base/LoginServletAndroid.java
@@ -1,23 +1,16 @@
 package com.github.akagawatsurunaki.novappro.android.stu.service.base;
 
-import cn.hutool.extra.servlet.ServletUtil;
 import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
-import com.alibaba.fastjson2.TypeReference;
-import com.github.akagawatsurunaki.novappro.model.database.User;
-import com.github.akagawatsurunaki.novappro.model.frontend.ServiceMessage;
-import com.github.akagawatsurunaki.novappro.model.frontend.ServiceResult;
 import com.github.akagawatsurunaki.novappro.service.base.LoginService;
 import com.github.akagawatsurunaki.novappro.util.ResponseUtil;
 import lombok.val;
-import org.apache.commons.lang3.tuple.Pair;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
 
 @WebServlet(name = "LoginServletAndroid", value = "/android/login")
 public class LoginServletAndroid extends HttpServlet {
